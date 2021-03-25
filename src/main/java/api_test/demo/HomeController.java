6d5adc454d64afb2f8861cde15e2dc48b057c89c
Florian Stadler <ram@ranch.com>
2021-03-25 12:24:53 +0100

    Setup with java files (with gitignore)

--- src/main/java/api_test/demo/HomeController.java
+++ src/main/java/api_test/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @RestController
 public class HomeController {
